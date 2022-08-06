9a4ac96a0e8d7a5996b0cfff3787c31b7bd16037
julian <julian_ars1@hotmail.com>
2022-08-06 11:55:20 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,57 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {Image} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import CoinsStack from './src/components/coins/CoinsStack';
+import FavoritesStack from './src/components/favorites/FavoritesStack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Colors from './src/res/colors';
+
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarStyle: {
+            backgroundColor: Colors.blackPearl,
+          },
+        }}>
+        <Tab.Screen
+          name="Coins"
+          component={CoinsStack}
+          options={{
+            tabBarIcon: ({size, color}) => (
+              <Image
+                style={{tintColor: color, width: size, height: size}}
+                source={require('./src/assets/bank.png')}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Favorites"
+          component={FavoritesStack}
+          options={{
+            tabBarIcon: ({size, color}) => (
+              <Image
+                style={{tintColor: color, width: size, height: size}}
+                source={require('./src/assets/star.png')}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
